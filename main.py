77b5b09f806005e377038c18b241a442428e66b7
Jonathan Adam Rico <51609531+jonathanadamrico@users.noreply.github.com>
2024-11-19 16:23:19 +0800

    FFT-1DCNN-AE

---
+++ main.py
@@ -0,0 +1,8 @@
+#main.py
+
+import os
+
+os.system("python3 run_run_1dcnn_ae_all_types.py")
+os.system("python3 run_infer_1dcnn_ae_all_types.py")
+os.system("python3 eval_model.py test")
+os.system("python3 eval_model.py final")
